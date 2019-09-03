867332f2c1796d4a3cb98201153c7046beff28c7
Ivanov Correa <adrianivanov@outlook.com>
2019-09-03 01:41:40 -0500

    Commit 001

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+	// El inyector
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			IAlimento alimento = null;
+
+			//alimento = new Pizza("Lechuga");
+			alimento = new Hamburguesa() { Tipo = "Cuarto Doble con Queso" };
+
+			// Inyectamos la dependencia a tavés del constructor
+			var persona = new Persona(alimento);
+
+			// Persona usa 
+			persona.Comer();
+
+            // Stop
+            Console.ReadKey();
+		}
+	}
+}
